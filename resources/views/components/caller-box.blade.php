38c901986e3da9e93eeca5e6496972cd4e1bb6cf
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-09-23 10:04:02 +0200

    feat: add caller box component with responsive design and styles

--- resources/views/components/caller-box.blade.php
+++ resources/views/components/caller-box.blade.php
@@ -1,16 +1,18 @@
-<div class="caller-box">
-
-    <div>
-        <p>Potrebujete poradiť?</p>
-        <h1>Zanechajte svoje číslo a čoskoro vám zavoláme späť.</h1>
+<div class="caller-box-section">
+    <div class="wrapper">
+        <div class="caller-box">
+            <div class="caller-box-text">
+                <p class="title">Potrebujete poradiť?</p>
+                <h2>Zanechajte svoje číslo a čoskoro vám zavoláme späť.</h2>
+            </div>
+            <div class="caller-box-form">
+                <form action="" method="POST">
+                    @csrf
+                    <input type="text" name="name" placeholder="Uveďte prosím svoje meno" required>
+                    <input type="tel" name="telephone" placeholder="Uveďte prosím svoje telefónne číslo" required>
+                    <button type="submit">Odoslať</button>
+                </form>
+            </div>
+        </div>
     </div>
-
-    <div>
-        <form action="" method="post">
-            <input type="text" name="" id="">
-            <input type="text" name="" id="">
-            <input type="submit" value="">
-        </form>
-    </div>
-
 </div>
