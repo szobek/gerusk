296fabe55ed5eeb97fbf28349f32d68561b57a23
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-09-23 10:06:51 +0200

    fix: correct placement of favicon link in the HTML head section

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<link rel="shortcut icon" href="/images/logo.png" type="image/png">
     <title>Geru</title>
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
