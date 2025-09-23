2de5e91c58b5fe860715c9de4d08b243d7e1e962
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-09-23 15:05:02 +0200

    feat: create Header component and refactor header section in welcome view

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,19 @@
+<div class="wrapper">
+
+            <div class="logo">
+
+                <a href="">
+                    <img src="/images/logo.png" alt="Geru logo">
+                </a>
+            </div>
+            <x-Nav />
+            <div class="caller-btn hide-in-small">
+                <a href="tel:+36303517871">Zavolať teraz</a>
+            </div>
+            <div class="break"></div>
+            <x-banner-text />
+            <div class="caller-btn">
+                <a href="tel:+36303517871">+36 30 351 78 71</a>
+            </div>
+        </div>
+        <div class="overlay"></div>
